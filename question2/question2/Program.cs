ba647ab79bd20cfc7708cae18bee5803511ba9d9
raphael ohana <rohana@g.jct.ac.il>
2019-11-11 14:43:14 +0200

    targuil 1 fini question 1 ( ligne 8) question 2 ( ligne 49)

--- question2/question2/Program.cs
+++ question2/question2/Program.cs
@@ -5,7 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Targil1_2Amiti
+namespace Targil1_1
+{
+    class Program
+    {
+        static Random r = new Random(DateTime.Now.Millisecond);
+        const int SIZE = 20;
+        static void Main(string[] args)
+        {
+            int[] A = new int[SIZE];
+            int[] B = new int[SIZE];
+            int[] C = new int[SIZE];
+            //init the arrays to random values
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = r.Next(18, 122);
+                B[i] = r.Next(18, 122);
+                C[i] = Math.Abs(A[i] - B[i]);
+            }
+            foreach (int item in A)
+            {
+                Console.Write("{0,3} ",item);
+            }
+            Console.WriteLine();
+            foreach (int item in B)
+            {
+                Console.Write("{0,3} ", item);
+            }
+            Console.WriteLine();
+            foreach (int item in C)
+            {
+                Console.Write("{0,3} ", item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+namespace Targil1_2
 {
     class Program
     {
@@ -138,3 +179,4 @@ namespace Targil1_2Amiti
     }
     
 }
+
